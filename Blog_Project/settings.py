072e8c02e9639371e05b2fafc1f474a3f745b070
Meshkat Ahammed <meshkatuddinahammed@outlook.com>
2022-10-25 19:08:03 +0600

    Creating MOdels

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login_app',
     'blog_app',
+    'address',
 ]
 
 MIDDLEWARE = [
--- login_app/models.py
+++ login_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from address.models import AddressField
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='user_profile')
+    profile_pic = models.ImageField(upload_to='static/media/images/profilepics')
+    address = AddressField(on_delete=models.CASCADE)
